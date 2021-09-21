49275990a22fcdb59500917928ed8bb0f4a0c31a
Desausag <dinar095@hotmail.com>
2021-09-21 14:54:11 +0300

    ex03 in progress

--- ex03/ClapTrap.hpp
+++ ex03/ClapTrap.hpp
@@ -13,10 +13,10 @@ public:
 	ClapTrap(string name);
 	~ClapTrap();
 	ClapTrap(const ClapTrap& src);
-	ClapTrap& operator=(const ClapTrap& src);
-	void			attack(std::string const & target);
-	void			takeDamage(unsigned int amount);
-	void			beRepaired(unsigned int amount);
+	ClapTrap&			operator=(const ClapTrap& src);
+	void				attack(std::string const & target);
+	void				takeDamage(unsigned int amount);
+	void				beRepaired(unsigned int amount);
 	unsigned int		getHitpoints();
 	unsigned int		getEnergyPoint();
 	unsigned int		getAttackDamage();
@@ -24,8 +24,8 @@ public:
 
 
 protected:
-	string m_name;
-	unsigned int m_hitpoints;
-	unsigned int m_energy_point;
-	unsigned int m_attack_dam;
+	string			m_name;
+	unsigned int	m_hitpoints;
+	unsigned int	m_energy_point;
+	unsigned int	m_attack_dam;
 };
--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -1,10 +1,8 @@
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap()
+DiamondTrap::DiamondTrap(string name) : FragTrap(name), ScavTrap(name)
 {
-	m_name = "Default";
-	this->ClapTrap::m_name = this->m_name;
-	m_name = ClapTrap::m_name + "_clap_name";
+	m_name = name;
 	m_hitpoints = FragTrap::m_hitpoints;
 	m_energy_point = ScavTrap::m_energy_point;
 	m_attack_dam = FragTrap::m_attack_damage;
